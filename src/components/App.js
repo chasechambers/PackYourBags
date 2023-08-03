36ad0748db155781cc9ad7f75c842b9b60692271
Chase <chambers.chase@gmail.com>
2023-08-03 13:07:08 -0400

    Added local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from './Logo'
 import Form from './Form'
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 
 export default function App(){
-  const [items, setItems] = useState([]);
+
+  const [items, setItems] = useState(function() {
+    const storedValues = localStorage.getItem('items')
+    return JSON.parse(storedValues) || [];
+  })
+  // const [items, setItems] = useState(function () {
+  //   const storedvalue = localStorage.getItem("items")
+  //   return JSON.parse(storedvalue);
+  // });
   
 
   function handleAddItems(item) {
     setItems(items=> [...items, item]);
+
+    // localStorage.setItem('items', JSON.stringify([...items, item]));
   }
 
   function handleDeleteItems(id) {
@@ -22,9 +32,12 @@ export default function App(){
 
   function handleClearList() {
     const confirmed = window.confirm('Are you sure you want to delete everything?');
-
     if (confirmed) setItems([]);
   }
+
+  useEffect(function(){
+    localStorage.setItem('items', JSON.stringify(items));
+  },[items])
 
   return (
     <div className="app">
